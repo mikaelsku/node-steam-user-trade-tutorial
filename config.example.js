f6431546d41197f24874d7ee670f164a29d8a981
mikaelsku <oskari.kuusela96@gmail.com>
2015-09-24 16:04:17 +0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,10 +1,26 @@
 // Copy this file to config.js before entering your information.
 var config = {
-	username: "",
+	username: "oskari511",
 	password: "",
-	greetMsg: "I'm a bot that accepts all your unwanted items.  If you would like TF2 crates from me, please type: !trade <series> <amount>.  For example, !trade 82 5 will give you 5 crates of series 82.",
-	admin: '76561197985756607', // Steam ID 64-bit integer
-	domain: "example.com" // Only used to request a new key if you don't have a one already. Update to your own domain.
+	greetMsg: " Hello! I am currently selling CSGO Keys for 1.85/ea and buying CSGO Keys for 1.75/ea using ONLY BITCOINS.
+ 
+ To buy keys from me:
+ 1) Simply send me a trade offer with the number you wish to buy
+ 2) I will evaluate the amount in BTC which is to be sent to me
+ 3) I will accept the offer when I detect the payment
+ 
+ To sell keys to me:
+ 1) Send me a trade offer WITH YOUR BITCOIN ADDRESS AS MESSAGE IN THE OFFER
+ 2) I will evaluate the amount in BTC which I am supposed to send to you
+ 3) You will be asked to confirm the trade
+ 4) I will send the BTC after accepting the offer
+ 
+ I WILL REMOVE YOU AFTER TRADING WITH YOU! Add me again to trade.
+ 
+ Thank You
+",
+	admin: '76561198034282287', // Steam ID 64-bit integer
+	domain: "wwww.opkeys.com" // Only used to request a new key if you don't have a one already. Update to your own domain.
 };
 
 module.exports = config;
